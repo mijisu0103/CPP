3bca6f0c772271debb9464682f7ef346d45b29a8
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-13 21:03:05 +0100

    main.cpp

---
+++ Programming Language/Class_Division of Classes/main.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+
+void main()
+{
+    Player p; // Create a Player object
+
+    p.Input(); // Call the Input function to gather player data
+    // Enter job name
+    // Enter attack power
+    // Enter defense power
+    // Enter health points
+
+    p.Output(); // Call the Output function to display player data
+    // Output the player information
+}
