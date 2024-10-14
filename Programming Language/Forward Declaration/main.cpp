51fe5f44e0f6adb64dc99fd661fc0938b06a0fc8
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:38:20 +0100

    main.cpp

---
+++ Programming Language/Forward Declaration/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "A.h" // Include A.h to use A class
+#include "B.h" // Include B.h to use B class
+using namespace std;
+
+int main() // Corrected main function signature
+{
+    A a; // Create an instance of A
+    B b; // Create an instance of B
+
+    // You can add more logic here to use instances of A and B
+
+    return 0; // Return success code
+}
